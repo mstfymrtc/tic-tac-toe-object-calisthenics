31488384354267d9b31b7b1fd05f1bffe0d714bf
Mustafa Yumurtacı <mustafa.yumurtaci@trendyol.com>
2022-06-20 15:01:41 +0300

    first commit

---
+++ src/main/java/com/example/tic/tac/toe/object/calisthenics/TicTacToeObjectCalisthenicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tic.tac.toe.object.calisthenics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeObjectCalisthenicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeObjectCalisthenicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tic/tac/toe/object/calisthenics/TicTacToeObjectCalisthenicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tic.tac.toe.object.calisthenics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeObjectCalisthenicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
